cca6c62fbb84aae0919d2f71166008e05a42e98a
zl <zl@itcast.cn>
2017-10-11 22:22:45 +0800

    登录修改成功

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,14 @@
 
-	NProgress.start();
+	$(function () {
+	//	从cookie中获取userinfo的信息,使用模板引擎获取到信息展示到侧边栏
+	//
+		if(location.pathname != "/dashboard/login"){
+			//从cookie中获取userinfo的信息
+			var userinfo = $.cookie("userinfo");
+			userinfo = JSON.parse(userinfo);
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+			//使用模板引擎将获取到的信息展示到侧边栏
+			var html = template("tpl", userinfo);
+			$("#pro").html(html);
+		}
+	})
